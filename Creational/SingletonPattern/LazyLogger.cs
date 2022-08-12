aede424fc56c677b8162f5865004d888d45b4a23
İlhami ÖZBAY <ilhami.ozbay@gmail.com>
2022-08-12 23:49:46 +0300

    singleton with lazy

---
+++ Creational/SingletonPattern/LazyLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    internal sealed class LazyLogger
+    {
+        private static readonly Lazy<LazyLogger> _lazyLogger = new Lazy<LazyLogger>(() => new LazyLogger());
+
+        private LazyLogger()
+        {
+            LoggerGuid = Guid.NewGuid();
+        }
+        //to prevent to create multi instance in multi-thread apps
+        public Guid LoggerGuid { get; private set; }
+
+        public static LazyLogger GetInstance()
+        {
+            return _lazyLogger.Value;
+        }
+    }
+}
--- Creational/SingletonPattern/Program.cs
+++ Creational/SingletonPattern/Program.cs
@@ -2,17 +2,27 @@
 
 using SingletonPattern;
 
-Logger logger1 = null;
-Logger logger2 = null;
+int testLength = 10;
+List<LazyLogger> loggerList = new List<LazyLogger>();
+List<Thread> threadList = new List<Thread>();
 
-Thread thread1 = new Thread(() => { logger1 = Logger.GetInstance(); });
-Thread thread2 = new Thread(() => { logger2 = Logger.GetInstance(); });
-thread1.Start();
-thread2.Start();
-thread1.Join();
-thread2.Join();
+for (int i = 0; i < testLength; i++)
+{
+    if (i == testLength) break;
+    threadList.Add(new Thread(() => { loggerList.Add(LazyLogger.GetInstance()); }));
+}
+foreach (var item in threadList)
+{
+    item.Start();
+}
+foreach (var item in threadList)
+{
+    item.Join();
+}
 
-Console.WriteLine("logger1==logger2 ? " + (logger1 == logger2));
-Console.WriteLine("logger1.Equals(logger2) ? " + (logger1?.Equals(logger2)));
-Console.WriteLine("logger1 Guid = " + logger1?.LoggerGuid);
-Console.WriteLine("logger2 Guid = " + logger2?.LoggerGuid);
+for (int i = 0; i < loggerList.Count - 1; i++)
+{
+    Console.WriteLine($"logger{i}==logger{i + 1} ? " + (loggerList[i] == loggerList[i + 1]));
+    Console.WriteLine($"logger{i}.Equals(logger{i + 1}) ? " + (loggerList[i]?.Equals(loggerList[i + 1])));
+    Console.WriteLine($"logger{i} Guid = " + loggerList[i]?.LoggerGuid);
+}
